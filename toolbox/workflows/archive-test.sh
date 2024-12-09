b7857d56ed694c813a8327e0ea1731aab7963e60
cs1867 <blackhatracing@gmail.com>
2024-12-09 16:01:18 -0500

    Update archive-test.sh

--- toolbox/workflows/archive-test.sh
+++ toolbox/workflows/archive-test.sh
@@ -2,27 +2,27 @@
 echo "Change directory to artifact DEBs"
 cd artifacts/DEBS
 
-echo "List available repositories"
-sudo apt update
-echo "apt-cache policy"
-sudo apt-cache policy
+#echo "List available repositories"
+#sudo apt update
+#echo "apt-cache policy"
+#sudo apt-cache policy
 
-echo "Create repository"
-sudo apt install -y dpkg-dev
-dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
+#echo "Create repository"
+#sudo apt install -y dpkg-dev
+#dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
 
-echo "Add local repository"
-echo "deb [trusted=yes] file://$(pwd) ./" | sudo tee /etc/apt/sources.list.d/local-repo.list
+#echo "Add local repository"
+#echo "deb [trusted=yes] file://$(pwd) ./" | sudo tee /etc/apt/sources.list.d/local-repo.list
 
-echo "Update APT and list repositories again"
-sudo apt update
-sudo apt-cache policy
+#echo "Update APT and list repositories again"
+#sudo apt update
+#sudo apt-cache policy
 
-echo "Install perl-shared package"
-sudo dpkg -i perl-shared*.deb
+#echo "Install perl-shared package"
+#sudo dpkg -i perl-shared*.deb
 
-echo "Change directory back to build"
-cd /build/
+#echo "Change directory back to build"
+#cd /build/
 
 echo "Display repository file"
 ls -al /etc/apt/sources.list.d/local-repo.list
