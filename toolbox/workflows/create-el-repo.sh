e1030ec375314106f43804f0c784adee90f07dda
cs1867 <blackhatracing@gmail.com>
2025-05-20 13:37:07 -0400

    Update create-el-repo.sh

--- toolbox/workflows/create-el-repo.sh
+++ toolbox/workflows/create-el-repo.sh
@@ -4,10 +4,11 @@ echo "Create a repo to be uploaded "
 echo " change dir to artifact RPM"
 cd artifacts/RPMS
 echo " ls rpm dir"
+mkdir unibuild-repo
 ls
 
 
 echo "create the repo "
-
+mkdir unibuild-repo
 unibuild gather ./unibuild-repo
 ls -al ./unibuild-repo
