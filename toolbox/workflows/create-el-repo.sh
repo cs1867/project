d8e91a9907e1890ef6551820a80c24c7a8f84a2f
cs1867 <blackhatracing@gmail.com>
2025-05-23 13:25:34 -0400

    Update create-el-repo.sh

--- toolbox/workflows/create-el-repo.sh
+++ toolbox/workflows/create-el-repo.sh
@@ -1,4 +1,7 @@
 #!/bin/sh -e
+BUILD_OS="$1"
+BUILD_BRANCH="$2"
+
  
 echo "Create a repo to be uploaded "
 cd artifacts 
@@ -7,5 +10,8 @@ ls
 pwd
 sudo dnf -y install createrepo
 
-createrepo RPMS
+LOCAL_PATH="/repo/nightly/el/"
+
+final_cache_path="$LOCAL_PATH$BUILD_OS/.cache"
+sudo createrepo --update --simple-md-filenames -c $final_cache_path -p -d $LOCAL_PATH/${BUILD_OS}/x86_64/perfsonar/$BUILD_BRANCH 
 ls -al RPMS
