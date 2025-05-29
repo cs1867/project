3cb2be26a490a2ea0030f463496cff508c121938
cs1867 <blackhatracing@gmail.com>
2025-05-28 20:42:57 -0400

    Update create-el-repo.sh

--- toolbox/workflows/create-el-repo.sh
+++ toolbox/workflows/create-el-repo.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -e
+
+echo "**  Run create el repo sh ** "
 file="/build/variables.txt"
 # Read the variables from the file
 source $file
@@ -23,3 +25,6 @@ LOCAL_PATH="/repo/nightly/el/"
 final_cache_path="/$repo/$os_dir/$el_version/.cache"
 sudo createrepo --update --simple-md-filenames -c $final_cache_path -p -d /$repo/$os_dir/$el_version/x86_64/perfsonar/$build_branch 
 ls -al RPMS
+
+
+echo "**  End create el repo sh ** "
