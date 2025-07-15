f5479b5c939a33d1de42725f99f4cecf065aac43
cs1867 <blackhatracing@gmail.com>
2025-07-15 13:44:37 -0600

    Update create-el-repo.sh

--- toolbox/workflows/create-el-repo.sh
+++ toolbox/workflows/create-el-repo.sh
@@ -39,6 +39,7 @@ final_cache_path="$repo/$os_dir/$el_version/.cache"
 echo "sudo create repo"
 echo "final cache path $final_cache_path"
 sudo createrepo --update --simple-md-filenames  -p -d RPMS
+ls -l ./artifacts/RPMS/repodata/
 ##sudo createrepo --update --simple-md-filenames -c $final_cache_path -p -d $repo/$os_dir/$el_version/x86_64/perfsonar/$build_branch
 echo "ls the RPMS"
 ls -al RPMS
